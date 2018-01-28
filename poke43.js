0a664bafc23d1c5fb58065334b72480bd7801097
Radoslav Peev <rpeev@ymail.com>
2018-01-28 08:48:26 +0200

    Show key secondary value hints and animate keys on tap/swipe

--- poke43.js
+++ poke43.js
@@ -91,8 +91,22 @@ class Editor {
 
 class Keyboard {
   constructor(el) {
+    let classes = el.classList;
+
     this._el = el;
-    this._el.classList.add('poke43-keyboard');
+    this._hammer = new Hammer(this._el);
+
+    classes.add('poke43-keyboard');
+
+    this._hammer.
+      get('swipe').
+      set({direction: Hammer.DIRECTION_ALL});
+
+    this._hammer.
+      on('tap', ev => {
+        ev.preventDefault(); // Prevent zoom on double-tap
+      }).
+      on('swipe', ev => {});
   }
 }
 
@@ -104,12 +118,15 @@ class Key {
     this._el = el;
     this[`_text${0}`] = ds.text;
     this[`_command${0}`] = ds.command;
+    this[`_hint${0}`] = ds.hint;
     for (let i = 1; i < 9; i++) {
       this[`_text${i}`] = ds[`text${i}`];
       this[`_command${i}`] = ds[`command${i}`];
+      this[`_hint${i}`] = ds[`hint${i}`];
     }
     this._hammer = new Hammer(this._el);
-    this._dispatch = this._dispatch4cross;
+    this._dispatchSwipe = this._dispatchSwipe4cross;
+    this._renderHints = this._renderHints4cross;
     this._flashDuration = 100;
 
     classes.add('poke43-key');
@@ -124,7 +141,7 @@ class Key {
 
         this.onCommand(ev);
       }).
-      on('swipe', ev => this._dispatch(ev.angle).call(this, ev));
+      on('swipe', ev => this._dispatchSwipe(ev.angle).call(this, ev));
 
     for (let k in opts) {
       if (k.match(/^on[A-Z]/)) {
@@ -133,31 +150,31 @@ class Key {
     }
   }
 
-  _dispatch4cross(angle) {
+  _dispatchSwipe4cross(angle) {
     if (angle > -135 && angle <= -45) {
       return this.onCommand1;
     } else if (angle > -45 && angle <= 45) {
-      return this.onCommand2;
+      return this.onCommand3;
     } else if (angle > 45 && angle <= 135) {
-      return this.onCommand3;
+      return this.onCommand5;
     }
 
-    return this.onCommand4;
+    return this.onCommand7;
   }
 
-  _dispatch4diag(angle) {
+  _dispatchSwipe4diag(angle) {
     if (angle > -90 && angle <= 0) {
-      return this.onCommand1;
-    } else if (angle > 0 && angle <= 90) {
       return this.onCommand2;
+    } else if (angle > 0 && angle <= 90) {
+      return this.onCommand4;
     } else if (angle > 90 && angle <= 180) {
-      return this.onCommand3;
+      return this.onCommand6;
     }
 
-    return this.onCommand4;
+    return this.onCommand8;
   }
 
-  _dispatch8(angle) {
+  _dispatchSwipe8(angle) {
     if (angle > -158 && angle <= -113) {
       return this.onCommand8;
     } else if (angle > -113 && angle <= -68) {
@@ -177,14 +194,54 @@ class Key {
     return this.onCommand7;
   }
 
+  _hint(i) {
+    return this[`_hint${i}`] || this[`_text${i}`] || '&nbsp;';
+  }
+
+  _renderHints4cross() {
+    this._el.innerHTML = `<table>
+<tr>
+  <td class="poke43-key-hint1">${this._hint(1)}</td>
+  <td rowspan="3" class="poke43-key-hint0">${this._hint(0)}</td>
+  <td class="poke43-key-hint3">${this._hint(3)}</td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td class="poke43-key-hint7">${this._hint(7)}</td>
+  <td class="poke43-key-hint5">${this._hint(5)}</td>
+</tr>
+    </table>`;
+  }
+
+  _renderHints4diag() {
+    this._el.innerHTML = `<table>
+<tr>
+  <td class="poke43-key-hint8">${this._hint(8)}</td>
+  <td rowspan="3" class="poke43-key-hint0">${this._hint(0)}</td>
+  <td class="poke43-key-hint2">${this._hint(2)}</td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td class="poke43-key-hint6">${this._hint(6)}</td>
+  <td class="poke43-key-hint4">${this._hint(4)}</td>
+</tr>
+    </table>`;
+  }
+
   _flash(ev, index, text, command) {
     let classes = this._el.classList;
 
     classes.add('poke43-key-flash');
-    classes.add(`key-flash${index}`);
+    classes.add(`poke43-key-flash${index}`);
     setTimeout(() => {
       classes.remove('poke43-key-flash');
-      classes.remove(`key-flash${index}`);
+      classes.remove(`poke43-key-flash${index}`);
     }, this._flashDuration);
   }
 
@@ -232,11 +289,14 @@ class EditorKeyChar extends EditorKey {
     super(el, editor);
 
     this._text1 = this._text1 || this._text0.toUpperCase();
-    this._command2 = this._command2 || 'moveForward';
-    this._text3 = this._text3 || ' ';
-    this._command4 = this._command4 || 'moveBackward';
+    this._command3 = this._command3 || 'moveForward';
+    this._text5 = this._text5 || ' ';
+    this._hint5 = this._hint5 || '\u2423';
+    this._command7 = this._command7 || 'moveBackward';
 
     classes.add('poke43-key-char');
+
+    this._renderHints();
   }
 }
 
@@ -247,12 +307,15 @@ class EditorKeyChar1 extends EditorKey {
     super(el, editor);
 
     this._text1 = this._text1 || this._text0.toUpperCase();
-    this._command2 = this._command2 || 'deleteForward';
-    this._text3 = this._text3 || '\n';
-    this._command4 = this._command4 || 'deleteBackward';
+    this._command3 = this._command3 || 'deleteForward';
+    this._text5 = this._text5 || '\n';
+    this._hint5 = this._hint5 || '\u21b2';
+    this._command7 = this._command7 || 'deleteBackward';
 
     classes.add('poke43-key-char');
     classes.add('poke43-key-danger');
+
+    this._renderHints();
   }
 }
 
@@ -262,9 +325,12 @@ class EditorKeySym extends EditorKey {
 
     super(el, editor);
 
-    this._dispatch = this._dispatch4diag;
+    this._dispatchSwipe = this._dispatchSwipe4diag;
+    this._renderHints = this._renderHints4diag;
 
     classes.add('poke43-key-sym');
+
+    this._renderHints();
   }
 }
 
